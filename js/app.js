e45cd03e2793d5ca3f0ad32bcaab078d2b36f1c9
Sayed Abdulla <Abdulla.s2002@gmail.com>
2025-07-11 17:31:39 +0300

    added addLetter function

--- js/app.js
+++ js/app.js
@@ -2,17 +2,37 @@ function init(){
 /*-------------------------------- Constants --------------------------------*/
 const target = ''
 const acceptedWords = []
+const guesses = [
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','','']
+]
 /*---------------------------- Variables (state) ----------------------------*/
-let guess
-let currentRow
+let currentRow = 0
 
 /*------------------------ Cached Element References ------------------------*/
 const letterEl = document.querySelectorAll(".guess-letter")
 const keyboardLetterEl = document.querySelectorAll(".keyboardLetter")
 /*-------------------------------- Functions --------------------------------*/
-
+function addLetter(letterEl){
+    console.log(letterEl.textContent)
+    for (let i = 0; i < guesses.length; i++){
+        if (guesses[currentRow][i] === ''){
+            guesses[currentRow][i] = letterEl.textContent
+            console.log(guesses[currentRow])
+            break
+        }
+    }
+}
+console.log(guesses)
+function render(){}
 /*----------------------------- Event Listeners -----------------------------*/
-
+keyboardLetterEl.forEach(letterEl => {
+    letterEl.addEventListener('click', () => addLetter(letterEl))
+});
 }
 
 document.addEventListener('DOMContentLoaded',init)
