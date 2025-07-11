6b1ff74256763950de6c85b9fa2f2f34b627c7ec
Sayed Abdulla <Abdulla.s2002@gmail.com>
2025-07-11 16:03:26 +0300

    added starting constants, variables and cached elements

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 function init(){
+/*-------------------------------- Constants --------------------------------*/
+const target = ''
+const acceptedWords = []
+/*---------------------------- Variables (state) ----------------------------*/
+let guess
+let currentRow
+
+/*------------------------ Cached Element References ------------------------*/
+const letterEl = document.querySelectorAll(".guess-letter")
+const keyboardLetterEl = document.querySelectorAll(".keyboardLetter")
+/*-------------------------------- Functions --------------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
 
 }
 
